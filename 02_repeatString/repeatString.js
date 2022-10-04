026d65d96a27e915333c99f09c9784a97997e0c4
Will OBrien <will.obrien93@gmail.com>
2022-10-03 20:57:10 -0400

    Complete Exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,8 @@
-const repeatString = function() {
-
+const repeatString = function(string, number) {
+    if(number < 0) {
+        return "ERROR"
+    }
+    return string.repeat(number);
 };
 
 // Do not edit below this line
