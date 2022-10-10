d3fe0d357bb23984de7f889ae76be8d613d34344
Will OBrien <will.obrien93@gmail.com>
2022-10-09 21:45:36 -0400

    Complete Exercise  03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,6 @@
 const reverseString = function(string) {
     let newString = '';
-    for (let i = string.length() - 1; i >= 0; i--) {
+    for (let i = string.length - 1; i >= 0; i--) {
         newString += string[i];
     }
     return newString;
