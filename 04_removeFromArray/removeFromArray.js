4f8b715d73366940c0cb8037473ac902f35f4372
Will OBrien <will.obrien93@gmail.com>
2022-10-09 22:19:26 -0400

    Complete Exercise 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...removals) {
+    for(let i = 0; i < removals.length; i++) {
+        removalIndex = array.indexOf(removals[i])
+            if (removalIndex != -1) {
+                array.splice(removalIndex,1)
+            }    
+        }
+    return array;
 };
 
 // Do not edit below this line
