08a3e8e8a287293b6af6e3b093e0bd58ab122b13
Will OBrien <will.obrien93@gmail.com>
2022-10-10 21:12:49 -0400

    Complete Exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,6 +1,24 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    // Input Validation
+    for (let i = 0; i < arguments.length; i++) {
+        if (arguments[i] < 0 || typeof(arguments[i]) != 'number') {
+            return 'ERROR'
+        }
 
-};
+    }
+    // Sum Calculation
+    sum = 0;
+    if (a <= b) {
+        for (let i = a; i <= b; i++) {
+            sum += i;
+        }
+    } else {
+        for (let i = b; i <= a; i++) {
+            sum += i;
+        }
+    }
+    return sum;
+}
 
 // Do not edit below this line
 module.exports = sumAll;
