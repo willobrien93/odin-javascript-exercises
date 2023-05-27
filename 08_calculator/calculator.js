d2f401337639af636ac3cf71001be7873c40b51e
Will OBrien <will.obrien93@gmail.com>
2023-05-26 23:03:57 -0400

    Completed exercise 8

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,38 @@
-const add = function() {
-	
+const add = function(a,b) {
+	return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a,b) {
+	return a - b;
 };
 
-const sum = function() {
-	
+const sum = function(numbers) {
+	returns = numbers.reduce(
+    (total, value) => total + value, 0
+  )
+  return returns;
 };
 
-const multiply = function() {
-
+const multiply = function(numbers) {
+  return numbers.reduce(
+    (total, value) => total * value
+  )
 };
 
-const power = function() {
-	
+const power = function(a,b) {
+	  let total = 1;
+    for (let i = 1; i <= b; i++){
+      total = total * a
+    }
+    return total;
 };
 
-const factorial = function() {
-	
+const factorial = function(a) {
+	total = 1
+  for (let i = a; i > 0; i--){
+    total = total * i
+  }
+  return total
 };
 
 // Do not edit below this line
